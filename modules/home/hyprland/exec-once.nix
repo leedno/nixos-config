cc0e48a2246c7a74dafe1379565a5c27057aef6f
leedno <leon.nordell04@gmail.com>
2025-12-16 18:24:41 +0100

    added mullvad browser

--- modules/home/hyprland/exec-once.nix
+++ modules/home/hyprland/exec-once.nix
@@ -38,7 +38,7 @@ in {
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start hyprpolkitagent"
         "pypr &"
-        "qs -c overview" # Start quickshell-overview daemon
+        # "qs -c overview" # Start quickshell-overview daemon
       ]
       ++ noctaliaExec ++ waybarExec;
   };
