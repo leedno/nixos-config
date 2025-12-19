c608950d68a075a480a0449fa7f4ab03f7aae9e2
leedno <leon.nordell04@gmail.com>
2025-12-19 23:22:37 +0100

    added up command to auto pull and rebuild system

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -60,6 +60,7 @@
       v = "nvim";
       c = "clear";
       fr = "nh os switch --hostname ${profile}";
+      up = "cd $HOME/leonos && git pull && nh os switch --hostname ${profile}";
       fu = "nh os switch --hostname ${profile} --update";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
