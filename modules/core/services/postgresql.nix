584b5fe6b30af2499c98ba07f63b777dd2023084
leedno <leon.nordell04@gmail.com>
2026-01-30 17:23:44 +0100

    postgresql settings edited

--- modules/core/services/postgresql.nix
+++ modules/core/services/postgresql.nix
@@ -6,11 +6,18 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_16;
-    # This automatically creates the database for your course
-    ensureDatabases = ["nodemovies"];
+
+    # Create user automatically
+    ensureUsers = [
+      {
+        name = "leon";
+        ensureClauses.superuser = true;
+        ensureClauses.login = true;
+      }
+    ];
+
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  user  address     method
       local all       all               trust
       host  all       all   127.0.0.1/32  trust
       host  all       all   ::1/128       trust
